a841e171392ef2e0c3bf92061444d51c63930ff6
Denis Malinin <melowetty@mail.ru>
2024-08-30 22:01:55 +0500

    add props and enable kafka

--- src/main/kotlin/ru/melowetty/notificationservice/NotificationServiceApplication.kt
+++ src/main/kotlin/ru/melowetty/notificationservice/NotificationServiceApplication.kt
@@ -3,9 +3,11 @@ package ru.melowetty.notificationservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.kafka.annotation.EnableKafka
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableKafka
 class NotificationServiceApplication
 
 fun main(args: Array<String>) {
