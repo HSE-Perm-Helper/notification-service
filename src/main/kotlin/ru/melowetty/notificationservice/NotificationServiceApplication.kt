6b3404387d1af511788df4d65a2a35353d069d52
Denis Malinin <melowetty@mail.ru>
2025-05-07 03:47:47 +0500

    refactoring: move main method in app class

--- src/main/kotlin/ru/melowetty/notificationservice/NotificationServiceApplication.kt
+++ src/main/kotlin/ru/melowetty/notificationservice/NotificationServiceApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class NotificationServiceApplication
-
-fun main(args: Array<String>) {
-    runApplication<NotificationServiceApplication>(*args)
+class NotificationServiceApplication {
+    fun main(args: Array<String>) {
+        runApplication<NotificationServiceApplication>(*args)
+    }
 }
